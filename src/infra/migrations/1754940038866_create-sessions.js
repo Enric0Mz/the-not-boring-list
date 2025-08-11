e1ccb9647992e3bb50b298c9ac1d1f8c03860878
Enrico Marquez <enricovmarquezz@gmail.com>
2025-08-11 16:27:54 -0300

    feat: include `sessions` table migration

---
+++ src/infra/migrations/1754940038866_create-sessions.js
@@ -0,0 +1,31 @@
+exports.up = (pgm) => {
+  pgm.createTable("sessions", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+    },
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+    token: {
+      type: "varchar(96)", // generated with crypto randomBytes(48)
+    },
+    user_ud: {
+      type: "integer",
+      notNull: true,
+      references: '"users"',
+      onDelete: "SET NULL",
+    },
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
+    },
+    updated_at: {
+      type: "timestamptz",
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
+    },
+  });
+};
