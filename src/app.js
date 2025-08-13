fd4c81f1fc4ce6852e89719c50ac6802f64a54ea
Enrico Marquez <enricovmarquezz@gmail.com>
2025-08-13 14:45:47 -0300

    feat: segregate `app.js` with `server.js` to avoid port errors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 import e from "express";
-import database from "./infra/database.js";
 
 const app = e();
-const port = 3000;
 
-app.get("/", async (req, res) => {
-  const result = await database.query("SELECT 1 + 1");
-  res.send(result.rows[0]);
+app.use(e.json());
+app.get("/health", async (req, res) => {
+  res.json({ ok: true });
 });
 
-app.listen(port, () => {
-  console.log(`EXAMPLE OF LISTENING IN PORT ${port}`);
-});
+export default app;
