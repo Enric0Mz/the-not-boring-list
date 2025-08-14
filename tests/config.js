77b37f9317e3eb383dfaf46dba51af49a8a549b7
Enrico Marquez <enricovmarquezz@gmail.com>
2025-08-14 15:22:35 -0300

    build: install and config `faker`

--- tests/config.js
+++ tests/config.js
@@ -1,4 +1,6 @@
 import database from "../src/infra/database";
+import { faker } from "@faker-js/faker";
+import User from "../src/models/User";
 
 async function clearDatabase() {
   return await database.query(` 
@@ -16,8 +18,18 @@ async function clearDatabase() {
     `); // Query provided by https://stackoverflow.com/users/939860/erwin-brandstetter
 }
 
+async function createUser(userObject) {
+  return await User.create({
+    username:
+      userObject.username || faker.internet.username().replace(/[_.-]g/, ""),
+    email: userObject.email || faker.internet.email(),
+    password: userObject.password || "validpassword",
+  });
+}
+
 const config = {
   clearDatabase,
+  createUser,
 };
 
 export default config;
