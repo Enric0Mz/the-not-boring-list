f7a59c79ee7d7f49a055f79df3f387599c4a680c
Enrico Marquez <enricovmarquezz@gmail.com>
2025-08-15 15:51:09 -0300

    feat: implement `getByEmail` method

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,24 @@
 import database from "../infra/database.js";
 
+async function getByEmail(email) {
+  return await runSelectQuery(email);
+
+  async function runSelectQuery(email) {
+    const text = `
+      SELECT
+        id, password
+      FROM 
+        users
+      WHERE
+        email = $1
+    `;
+    const values = [email];
+
+    const result = await database.query({ text, values });
+    return await result.rows[0];
+  }
+}
+
 async function create(userObject) {
   return await runCreateQuery(
     userObject.username,
@@ -15,10 +34,13 @@ async function create(userObject) {
     `;
     const values = [username, email, password];
     const result = await database.query({ text, values });
+    if (!result.rows[0]) {
+      throw Error("User not found"); // TODO implement error handling
+    }
     return await result.rows[0];
   }
 }
 
-const User = { create };
+const User = { create, getByEmail };
 
 export default User;
