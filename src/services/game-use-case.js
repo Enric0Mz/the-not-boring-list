5b3620776ecd7e52948d6434fc2271a61c87b8a4
Enrico Marquez <enricovmarquezz@gmail.com>
2025-08-21 14:47:23 -0300

    refactor: modularize `search` use case with small functions

--- src/services/game-use-case.js
+++ src/services/game-use-case.js
@@ -2,17 +2,22 @@ import rawg from "./apis/rawg.js";
 
 async function search(searchParams) {
   const games = await rawg.searchGame(searchParams);
-  let result = [];
-  games.results.forEach((element) => {
-    result.push({
-      id: element.id,
-      name: element.name,
-      image: element.background_image,
-      hours_to_beat: element.playtime,
-      score: element.metacritic,
-    });
-  });
+  const result = extractPayload(games);
   return { data: result };
+
+  function extractPayload(games) {
+    let result = [];
+    games.results.forEach((element) => {
+      result.push({
+        id: element.id,
+        name: element.name,
+        image: element.background_image,
+        hours_to_beat: element.playtime,
+        score: element.metacritic,
+      });
+    });
+    return result;
+  }
 }
 
 const gameUseCase = { search };
