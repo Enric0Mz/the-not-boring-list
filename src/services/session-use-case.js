f476ac10f276a051f02ae35492f3fb8817b7de19
Enrico Marquez <enricovmarquezz@gmail.com>
2025-09-16 15:32:28 -0300

    fix: include validation to check if user exist in `session` `POST` route

--- src/services/session-use-case.js
+++ src/services/session-use-case.js
@@ -2,11 +2,18 @@ import genRandomBytes from "./utils/random-bytes.js";
 import Session from "#src/models/Session.js";
 import User from "#src/models/User.js";
 import password from "#src/infra/security/password.js";
+import { NotFoundError } from "#src/infra/errors.js";
 
 const TOKEN_EXPIRATION_IN_MILLISECONDS = 60 * 60 * 24 * 15 * 1000; // 15 days
 
 async function createSession(userObject) {
   const validUser = await User.getByEmail(userObject.email);
+
+  if (!validUser) {
+    throw new NotFoundError(
+      `The value ${userObject.email} was not found in database`,
+    );
+  }
 
   await validatePassword(userObject.password, validUser.password);
 
